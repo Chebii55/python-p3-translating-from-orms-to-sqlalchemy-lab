7cdf8c2659cf0200ad54e9be6a625453610fbceb
Sheila Chebii <shillerchier055@gmail.com>
2024-01-08 15:14:34 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,45 @@
 from models import Dog
+from sqlalchemy import (create_engine)
 
-def create_table(base):
-    pass
+def create_table(base,engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs=session.query(Dog).all()
+    return [dog for dog in dogs]
 
 def find_by_name(session, name):
-    pass
+    dogs=session.query(Dog).filter(Dog.name.like(name)).all()
+    
+    for dog in dogs:
+        return dog
+
+    
 
 def find_by_id(session, id):
-    pass
+    dogs=session.query(Dog).filter(Dog.id.like(id)).all()
+    
+    for dog in dogs:
+        return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dogs=session.query(Dog).filter(Dog.name.like(name),Dog.breed.like(breed)).all()
+    
+    for dog in dogs:
+        return dog
+    
 
 def update_breed(session, dog, breed):
-    pass
+    x=session.query(Dog).filter(Dog.name.like(f'%{dog}%')).first()
+    if dog:
+        dog.breed = breed
+        session.commit()
+        return dog
+    else:
+        print(f"No dog found with name like {dog}")
+        return None
+
